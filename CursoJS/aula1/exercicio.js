0573f0d4c727b152587482d7fb4de47e0e9ff726
NICKN1 <57010176+NICKN1@users.noreply.github.com>
2020-08-17 10:45:24 -0300

    Primeira aula
    Aprendendo a usar console.log

---
+++ CursoJS/aula1/exercicio.js
@@ -0,0 +1,2 @@
+console.log('Meu nome é "Junior". Estou aprendendo Javascript às', 10, 'da manhã.');
+console.log("Outro", "exemplo", "pra", "gente", "ver,", 10);
---
+++ CursoJS/aula1/index.js
@@ -0,0 +1,5 @@
+console.log(15);
+console.log('Junior "Arrais"'); // String = Texto
+console.log("Junior 'Arrais'"); // Number = Número
+console.log(`'JUnior' "Arrais`);
+console.log(15, "junior", 45);
