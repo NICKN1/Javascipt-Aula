ef1d7f83a1cf9e43d838649a4879751807314f27
NICKN1 <57010176+NICKN1@users.noreply.github.com>
2020-08-17 10:58:05 -0300

    Comentários
    Como fazer cometários de linha e de blocos

---
+++ CursoJS/aula2/comentarios.js
@@ -0,0 +1,15 @@
+// eu quero fazer um comentário no meu código!!
+/*console.log('Jonas!!!'); 
+console.log('1');*/
+//console.log('2');
+console.log('3');
+console.log("4"); // não altero nada aqui
+
+// comentário de uma linha se usa "//"
+/* comentário
+de 
+bloco
+assim
+se usa
+o "/*" 
+*/
