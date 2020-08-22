6a902c297c262ba37a263a76730e0a11d5ed6146
NICKN1 <57010176+NICKN1@users.noreply.github.com>
2020-08-22 02:06:23 -0300

    Variáveis com let
    Regras e conceitos usando let

---
+++ CursoJS/aula4/consoles.js
@@ -0,0 +1,8 @@
+let eu = 'i';
+
+
+
+console.log('My friends');
+console.log(eu, "don't have many time");
+console.log("So... ", eu,"'ve to sayed");
+console.log(eu, "love you all!");
---
+++ CursoJS/aula4/index.js
@@ -0,0 +1,20 @@
+let nome; // declarou a variável
+nome = 'i'; //Inicializando a variável
+console.log(nome);
+nome = 'you'; //delcarar outro valor
+console.log(nome); //executar o novo valor
+
+// Não podemos criar variáveis com palavras reservadas
+// Variáveis precisam ter nomes significativos
+// Não pode começar o nome de uma variável com um número
+// Não podem conter espaços ou traços nos nomes das variáveis
+/* Utilizamos camelCase (Começa com letra minúscula e depois maiúscula)
+   Ex: let nomeCompleto = 'João Inácio';
+*/ 
+/* Variáveis são case-sensitive (Os tamanhos das letras diferenciam
+    o tamanho das variáveis)
+    Ex: let nomeCliente = 'João';
+        let nomecliente = 'Pedro';
+*/
+// Não podemos redeclarar variáveis com let, podendo mudar seu valor
+// NÃO UTILIZE VAR, UTILIZE LET
