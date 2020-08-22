7819d77f260b5c87e6b67836755741ab8bc5842a
NICKN1 <57010176+NICKN1@users.noreply.github.com>
2020-08-22 02:09:09 -0300

    Alterando o texto. Errei no meu inglês '-'

--- CursoJS/aula4/consoles.js
+++ CursoJS/aula4/consoles.js
@@ -4,5 +4,5 @@ let eu = 'i';
 
 console.log('My friends');
 console.log(eu, "don't have many time");
-console.log("So... ", eu,"'ve to sayed");
+console.log("So... ", eu,"'ve to say");
 console.log(eu, "love you all!");
