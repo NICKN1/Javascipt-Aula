45c9042feae9746ba72a966f47d2eedc01e5c603
NICKN1 <57010176+NICKN1@users.noreply.github.com>
2020-08-24 01:24:07 -0300

    Diferenças entre Var e Let

---
+++ CursoJS/aula6/index.js
@@ -0,0 +1,11 @@
+// ECMAScript é uma padronização da linguagem JavaScript
+
+var nome = 'João';
+var nome = 'Pedro'; //Para alterar, necessita declarar var
+
+console.log(nome);
+
+let nomes = 'João';
+nomes = 'Pedro'; //Para alterar, não necessita declarar let
+
+console.log(nomes);
