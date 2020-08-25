637f461eac4b053377db1ee9ccad677f910e56e2
Junior Arrais <57010176+NICKN1@users.noreply.github.com>
2020-08-25 00:50:41 -0300

    Operadores Aritméticos

---
+++ CursoJS/aula8/index.js
@@ -0,0 +1,12 @@
+/*
+ Aritméticos
+  + Adição / concatenação
+  - / * 
+ */
+const num1 = 5;
+const num2 = 15;
+console.log(num1 + num2); // exemplo de adcição
+
+const num3 = '5';
+const num4 = 15;
+console.log(num3 + num4); // exemplo de Concatenação
