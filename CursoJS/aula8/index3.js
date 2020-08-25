ac7107f5585a65e4108807a604ece9255d362372
Junior Arrais <57010176+NICKN1@users.noreply.github.com>
2020-08-25 00:49:52 -0300

    Delete index3.js

--- CursoJS/aula8/index3.js
+++
@@ -1,9 +0,0 @@
-// Converter String para Number
-const num1 = 10;
-const num2 = parseInt('5'); /* Transforma em um inteiro */
-const num3 = parseFloat('5.4') /* Transforma em decimal */
-const num4 = Number('5'); /* Transforma em número */
-console.log(num1 + num2 + num3 + num4);
-console.log(typeof num2);
-console.log(typeof num3);
-console.log(typeof num4);
